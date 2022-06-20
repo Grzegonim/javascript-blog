fe502dda81b8bcf123e54b80f7fa858bd64882cd
Grzegorz Woźniak <grzegorz.wozniak1989@gmail.com>
2022-06-20 19:00:14 +0200

    Add eslint

--- js/script.js
+++ js/script.js
@@ -5,46 +5,46 @@
     console.log('links:', links);
   }); */
 
-  const titleClickHandler = function(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
+const titleClickHandler = function(event){
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
     
   // [DONE] remove class .active from all article links 
-    const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for (let activeLink of activeLinks){
-      activeLink.classList.remove('active');
+  for (let activeLink of activeLinks){
+    activeLink.classList.remove('active');
   }
   // [DONE] add class .active to the clicked link 
-    console.log('clickedElement:', clickedElement);
+  console.log('clickedElement:', clickedElement);
 
-    clickedElement.classList.add('active');
+  clickedElement.classList.add('active');
   // [DONE] remove class .active from all article
-    const activeArticles = document.querySelectorAll('article.active');
+  const activeArticles = document.querySelectorAll('article.active');
 
-    for (let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
+  for (let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
   }
   // [DONE] get href atribute from clicked link
-    const hrefValue = clickedElement.getAttribute('href');
+  const hrefValue = clickedElement.getAttribute('href');
   
   // [DONE] find the correct article using the selector (value of href)
-    const targetArticle = document.querySelector(hrefValue);
+  const targetArticle = document.querySelector(hrefValue);
 
   // [DONE] add class .active to the correct article
-    targetArticle.classList.add('active');  
-}
+  targetArticle.classList.add('active');  
+};
 
 const optArticleSelector = '.post',
-optTitleSelector = '.post-title',
-optTitleListSelector = '.titles';
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
 function generateTitleLinks(){
-const titleList = document.querySelector(optTitleListSelector);
-titleList.innerHTML = '';
-const articles = document.querySelectorAll(optArticleSelector);
-let html = '';
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
   for (let article of articles){
     const articleId = article.getAttribute('id');
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
@@ -57,10 +57,10 @@ let html = '';
 }
 generateTitleLinks();
 
-  const links = document.querySelectorAll('.titles a');
-  console.log(links);
-  for(let link of links){
-    link.addEventListener('click', titleClickHandler);
-  }
+const links = document.querySelectorAll('.titles a');
+console.log(links);
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
     
 
