d4dde130e4478388ee5c730671a6405591a24538
Grzegorz Woźniak <grzegorz.wozniak1989@gmail.com>
2022-06-28 20:27:29 +0200

    Add tags&authors list

--- js/script.js
+++ js/script.js
@@ -36,7 +36,11 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optArticleAuthorSelector = '.post .post-author';
+  optArticleAuthorSelector = '.post .post-author',
+  optTagsListSelector = '.tags.list',
+  optCloudClassCount = 5,
+  optCloudClassPrefix = 'tag-size-',
+  optAuthorsListSelector = '.list.authors';
   
 function generateTitleLinks(customSelector = ''){
   const titleList = document.querySelector(optTitleListSelector);
@@ -59,7 +63,33 @@ for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
 
+function calculateTagClass(count, params){
+  const normalizedCount = count - params.min;
+  const normalizedMax = params.max - params.min;
+  const percentage = normalizedCount / normalizedMax;
+  const classNumber = Math.floor(percentage * (optCloudClassCount - 1) + 1);
+  return classNumber;
+}
+
+function calculateTagsParams(tags){
+  const params = {
+    max:0,
+    min:999999,
+  }
+  for (let tag in tags){
+    if(tags[tag] > params.max){
+      params.max = tags[tag];
+    }
+    if(tags[tag] < params.min){
+      params.min = tags[tag];
+    }
+  }
+  return params;
+}
+
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty object */
+  let allTags = {};
   /* [DONE]find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /* [DONE]START LOOP: for every article: */
@@ -78,12 +108,33 @@ function generateTags(){
       const linkHTML = '<li><a href="#tag-' + tag + '">' + tag + '\xa0' + '</a></li>';  
       /* [DONE]add generated code to html variable */
       html = html + linkHTML;
+      /* [NEW] check if this link is NOT already in allTags */
+      if(!allTags[tag]) {
+        /* [NEW] add generated code to allTags array */
+        allTags[tag] = 1;
+      } else {
+        allTags[tag]++;
+      }
     }
     /* [DONE]END LOOP: for each tag */
     /* [DONE]insert HTML of all the links into the tags wrapper */
     articlesTags.innerHTML = html;  
   }
   /* [DONE]END LOOP: for every article: */
+   /* [NEW] find list of tags in right column */
+   const tagList = document.querySelector(optTagsListSelector);
+   /* [NEW] create variable for all links HTML code */
+   const tagsParams = calculateTagsParams(allTags);
+   let allTagsHTML = '';
+   /* [NEW] START LOOP: for each tag in allTags: */
+   for(let tag in allTags){
+    /* [NEW] generate code of a link and add it to allTagsHTML */
+    allTagsHTML += '<li><a href="#tag-' + tag + '" class="' + optCloudClassPrefix + calculateTagClass(allTags[tag], tagsParams) + '">' + tag + '\xa0' + '</a></li>';
+   }
+   /* [NEW] END LOOP: for each tag in allTags: */
+   /* [NEW] add html from allTags to tagList */
+   tagList.innerHTML = allTagsHTML;
+   
 }
 generateTags();
 
@@ -130,6 +181,7 @@ function addClickListenersToTags(){
 addClickListenersToTags();
 
 function generateAuthors(){
+  let allAuthors = {};
   const articles = document.querySelectorAll(optArticleSelector);
   for (let article of articles){
     const authorWrapper = article.querySelector(optArticleAuthorSelector);
@@ -138,7 +190,18 @@ function generateAuthors(){
     const linkHTML = '<p class="post-author"><a href="#author-' + targetAuthor + '">by ' + targetAuthor + '</a></p>';
     html = html + linkHTML;
     authorWrapper.innerHTML = html;
-  }  
+    if (!allAuthors[targetAuthor]){
+      allAuthors[targetAuthor] = 1;
+    } else {
+      allAuthors[targetAuthor]++;
+    }
+  }
+  const wrapperListAuthors = document.querySelector(optAuthorsListSelector);
+  let allAuthorshtml = '';
+  for (let author in allAuthors){
+    allAuthorshtml += '<li><a href="#author-' + author + '">' + author + ' (' + allAuthors[author] + ')' + '</a></li>';
+  }
+  wrapperListAuthors.innerHTML = allAuthorshtml;
 }
 generateAuthors();
 
@@ -165,3 +228,4 @@ function addClickListenersToAuthors(){
   }
 }
 addClickListenersToAuthors();
+
